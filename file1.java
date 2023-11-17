0dee4a536ac46bf03fc7e20ad95a8521549e274f
Devlin <devlin.venedict@gmail.com>
2023-11-17 09:40:06 +0700

    update file1 and file2

--- file1.java
+++ file1.java
@@ -1,3 +1,5 @@
 public class file1 {
-    
+    public static void main(String[] args) {
+        System.out.println("ini file 1");
+    }
 }
--- file2.java
+++ file2.java
@@ -1,3 +1,6 @@
-public class file2{
-    
+public class file2 {
+    public static void main(String[] args) {
+        System.out.println("ini file 2");
+    }
+
 }
